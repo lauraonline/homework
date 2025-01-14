156f00f901c30b169706cffc6602743e7e2750fb
Laura Alves <lauraalves.contato@gmail.com>
2025-01-14 12:53:00 -0300

    update matrizquadrada.cpp

--- Algoritmos e Programacao 1 de 6/13-01-2025/matrizquadrada.cpp
+++ Algoritmos e Programacao 1 de 6/13-01-2025/matrizquadrada.cpp
@@ -4,20 +4,21 @@ using namespace std;
 
 main() {
     setlocale (LC_ALL, "Portuguese");
-    int i, j, mt, mj, tam, imparesabaixoenadp;
-    float soma=0, somadp=0, maiorabaixoenadp;
+    // saída: matriz final, soma, média, no. de ímpares abaixo e na diagonal secundária, soma dos valores da diagonal principal, maior valor abaixo e na diagonal principal 
+    int i, j, mi, mj, tam, imparesabds=0; // linha, coluna, linha do maior, coluna do maior, tamanho da matriz, número de ímpares abaixo e na diagonal secundária
+    float soma=0, somadp=0, maiorabdp; // soma da matriz, soma dos valores da diagonal principal, maior valor abaixo e na diagonal principal
     cout<<"Insira o tamanho da matriz: ";
     cin>>tam;
-    float mat[tam][tam];
+    int mat[tam][tam];
     for (i = 0; i <= tam-1; i++) {
         for (j = 0; j <= tam-1; j++) {
-            cout<<"Insira o elemento ["<<i<<"]["<<j<<"]: ";
+            cout<<"Insira o elemento ["<<i+1<<"]["<<j+1<<"]: ";
             cin>>mat[i][j];
         }
     }
     cout<<endl;
-    maiorabaixoenadp = mat[0][0];
-    mt = 0;
+    maiorabdp = mat[0][0];
+    mi = 0;
     mj = 0;
     cout<<"Matriz final:"<<endl;
     for (i = 0; i <= tam-1; i++) {
@@ -27,12 +28,25 @@ main() {
             if (i == j) {
                 somadp = somadp + mat[i][j];
             }
+            if (i + j == tam-1 || i + j >= tam) {
+                if (mat[i][j] % 2 != 0) {
+                    imparesabds++;
+                }
+            }
             if (i == j || i > j) {
-                
+                if (mat[i][j] > maiorabdp) {
+                    maiorabdp = mat[i][j];
+                    mi = i;
+                    mj = j;
+                }
             }
         }
         cout<<endl;
     }
-
+    cout<<"Soma da matriz: "<<soma<<endl;
+    cout<<"Média da matriz: "<<soma/(tam*2.0)<<endl;
+    cout<<"Número de ímpares abaixo e na diagonal secundária: "<<imparesabds<<endl;
+    cout<<"Soma da diagonal principal: "<<somadp<<endl;
+    cout<<"Maior valor abaixo e na diagonal principal: "<<maiorabdp<<endl;
     system("pause");
 }
